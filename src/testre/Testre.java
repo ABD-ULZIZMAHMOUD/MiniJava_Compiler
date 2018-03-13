5125b6f3336e8f82e71ab53486ee5e161d51254b
ABD-ULZIZMAHMOUD <zizomody2511@gmail.com>
2018-03-13 18:22:12 +0200

    Third Commit

--- src/testre/Testre.java
+++ src/testre/Testre.java
@@ -5,6 +5,7 @@
  */
 package testre;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import jdk.nashorn.internal.runtime.regexp.RegExp;
@@ -22,12 +23,27 @@ public class Testre {
       // String to be scanned to find the pattern.
      // TokenLabel t =new TokenLabel("label", "token");
        // System.err.println(t.initializeTokensArray().get(2).toString());
-        System.out.println(TestStringAgenstRegEX.test("int intvalue = 10+5;"));;
+       ArrayList<TokenLabel> output = new ArrayList<>();
+       output= TestStringAgenstRegEX.test("class class1 {\n" +
+"int value = 3;\n" +
+"String text = \"int x = 1\";\n" +
+"/*\n" +
+"if (num < 1)\n" +
+"num = num +1 ;\n" +
+"*/\n" +
+"private void print()	\n" +
+"{\n" +
+"System.out.println(\"/*hello*/\");	\n" +
+"}\n" +
+"};");
+       for (int i=0;i<output.size();i++){
+           System.out.println(output.get(i));
+       }
         
         Map<Integer,String> z=new HashMap<Integer,String>();
 z.put(2, "mody");
 z.put(1, "zizo");
         System.out.println(z.toString());
-   }
+        int _5=5;   }
 }
     
