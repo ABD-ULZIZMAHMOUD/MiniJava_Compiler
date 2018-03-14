90ab8d0df91b9dacd6aced2b445ee3b30fe4f4d0
PC\mazen <mazen.mohamed.967@gmail.com>
2018-03-14 21:02:43 +0200

    TokenLabel updates v2

--- src/Lexer/TokenLabel.java
+++ src/Lexer/TokenLabel.java
@@ -35,195 +35,176 @@ import java.util.ArrayList;
     
     
     
-     static ArrayList<TokenLabel> initializeTokensArray(){
+     static ArrayList<TokenLabel> initializeTokensArray()
+     {
         ArrayList<TokenLabel> tokenLabels = new ArrayList<>();
         
-        ///////////////////////
-     String EOF = "\n" 
-     , IF = "\\bif\\b"
-     , ELSE = "\\belse\\b"
-     , WHILE = "\\bwhile\\b"
-     , MAIN = "\\bmain\\b"
-     , VOID = "\\bvoid\\b"
-     , THIS = "\\bthis\\b"
-     , CLASS = "\\bclass\\b"
-     , PUBLIC = "\\bpublic\\b"
-     , RETURN = "\\breturn\\b"
-     , STATIC = "\\bstatic\\b"
-     , NEW = "\\bnew\\b"
-    
-     , TRUE = "\\btrue\\b"
-     , FALSE = "\\bfalse\\b"
-    
-     , LENGTH = "\\blength\\b"
-    
-     , EXTENDS = "\\bextends\\b"
-     , SYSTEM_OUT_PRINTLN = "\\bSystem\\.out\\.println\\b"
-     , INTEGRAL_LITERAL = "\\b(-?)\\d+\\b"
-     , FLOAT_LITERAL = "\\b\\d+\\.\\d+\\b"
-     , STRING_LITERAL = "\"\\w*\""
-     , ID = "\\b[a-zA-Z]\\w*\\b"
-     //,ID ="[a-zA-Z\\$\\_][a-zA-Z0-9\\$\\_]*"
-     , SQUOTE = "\'"
-     , DQUOTE = "\""
-     , COMMA = ","
-     , SEMICOLON = ";"
-     , DOT = "\\."
-    
-     , A_CHAR = "\'\\w\'"
-    
-     , STRING = "\\bstring\\b"
-     , FLOAT = "\\bfloat\\b"
-     , CHARACTER = "\\bchar\\b"
-     , BOOLEAN = "\\bboolean\\b"
-     , INT = "\\bint\\b"
-    
-    
-     , COMMENT1 = "\\/\\/.*"
-     , COMMENT2 = "\\/\\*.*\\*\\/"
-     , COMMENT_L = "\\/\\*"
-     , COMMENT_R = "\\*\\/"
-    
-     , PLUS = "\\+"
-     , MINUS = "\\-"
-     , MULTIPLY = "\\*"
-     , EQUAL = "\\="
-     , DIVITION = "\\/"
-    
-     , LEFT_CURLY_B = "\\{"
-     , RIGHT_CURLY_B = "\\}"
-     , LEFT_SQUARE_B = "\\["
-     , RIGHT_SQUARE_B = "\\]"
-     , LEFT_ROUND_B = "\\("
-     , RIGHT_ROUND_B = "\\)"
-     , LESSTHAN = ">"
-     , GREATERTHAN = "<"
-    
-     , NOT = "!"
-     , AND = "&&"
-     , OR = "\\|\\|";
+        ////////////////////////////////////////////////////////////////////////
+     String EOL = "\n" 
+     
+            /** Mazen STARTED FROM HERE  **/ 
+	   
+	   , MOD = "\\%"        	// check
+	   , LESSTHAN = "\\<"		// check
+	   , GREATERTHAN = "\\>"	// check
+           , LESS_EQ = "\\<\\="		// check
+	   , GREATER_EQ = "\\>\\="	// check
+	   , IF = "\\bif\\b"
+           , INT = "\\bint\\b"
+           , ELSE = "\\belse\\b"
+           , MAIN = "\\bmain\\b"
+           , THIS = "\\bthis\\b"
+           , TRUE = "\\btrue\\b"
+	   , VOID = "\\bvoid\\b"
+           , CLASS = "\\bclass\\b"
+           , FALSE = "\\bfalse\\b"
+	   , WHILE = "\\bwhile\\b"
+	   , LENGTH = "\\blength\\b"
+           , PUBLIC = "\\bpublic\\b"
+	   , PRIVATE = "\\bprivate\\b"
+	   , PROTECTED = "\\bprotected\\b"
+           , RETURN = "\\breturn\\b"
+       
+            /// Mazen END HERE
+	   
+            /** AZZAM STARTED FROM HERE  **/ 
+	   
+	   , STATIC = "\\bstatic\\b"
+	   , NEW = "\\bnew\\b"
+	   , STRING = "\\bstring\\b"
+           , FLOAT = "\\bfloat\\b"
+	   , CHARACTER = "\\bchar\\b"
+	   , BOOLEAN = "\\bboolean\\b"
+           , EXTENDS = "\\bextends\\b"
+           , NULL = "\\bnull\\b"
+           , ABSTRACT = "\\babstract\\b" 
+           , ASSERT = "\\bassert\\b"
+           , BREAK = "\\bbreak\\b"
+           , CONTINUE = "\\bcontinue\\b"
+           , BYTE = "\\bbyte\\b"    
+           , CASE = "\\bcase\\b" 
+           , CATCH = "\\bcatch\\b"
+           , CONST = "\\bconst\\b"
+           , DEFAULT = "\\bdefault\\b"
+           , DO = "\\bdo\\b"
+           , DOUBLE = "\\bdouble\\b"
+           , ENUM = "\\benum\\b"
+           , FINAL = "\\bfinal\\b"  
+             
+            /// AZZAM END HERE      
+             
+     ;
     
     //==========================================================================
-     String EOF_LABEL = "EOF"
+     String EOL_LABEL = "EOL"
+   
+	 
+            /** Mazen STARTED FROM HERE  **/ 
+	   
+	   , MOD_LABEL = "MOD"        	
+	   , LESSTHAN_LABEL = "LESSTHAN"		
+	   , GREATERTHAN_LABEL = "GREATERTHAN"
+           , LESS_EQ_LABEL = "LESS_EQ"
+	   , GREATER_EQ_LABEL = "GREATER_EQ"
+	   , IF_LABEL = "IF"
+           , INT_LABEL = "INT"
+           , ELSE_LABEL = "ELSE"
+           , MAIN_LABEL = "MAIN"
+           , THIS_LABEL = "THIS"
+           , TRUE_LABEL = "TRUE"
+	   , VOID_LABEL = "VOID"
+           , CLASS_LABEL = "CLASS"
+           , FALSE_LABEL = "FALSE"
+	   , WHILE_LABEL = "WHILE"
+	   , LENGTH_LABEL = "LENGTH"
+           , PUBLIC_LABEL = "PUBLIC"
+	   , PRIVATE_LABEL = "PRIVATE"
+	   , PROTECTED_LABEL = "PROTECTED"
+           , RETURN_LABEL = "RETURN"
+       
+            /// Mazen END HERE
     
-     , IF_LABEL = "IF"
-     , ELSE_LABEL = "ELSE"
-     , WHILE_LABEL = "WHILE"
-    
-     , MAIN_LABEL = "MAIN"
-     , VOID_LABEL = "VOID"
-     , THIS_LABEL = "THIS"
-     , CLASS_LABEL = "CLASS"
-     , PUBLIC_LABEL = "PUBLIC "
-     , RETURN_LABEL = "RETURN"
-     , STATIC_LABEL = "STATIC"
-     , NEW_LABEL = "NEW"
-    
-     , TRUE_LABEL = "TRUE"
-     , FALSE_LABEL = "FALSE"
-    
-     , LENGTH_LABEL = "LENGTH"
-    
-     , EXTENDS_LABEL = "EXTENDS"
-     , SYSTEM_OUT_PRINTLN_LABEL = "SYSTEM_OUT_PRINTLN"
-     , INTEGRAL_LITERAL_LABEL = "INTEGRAL_LITERAL"
-     , FLOAT_LITERAL_LABEL = "FLOAT_LITERAL"
-     , STRING_LITERAL_LABEL = "STRING_LITERAL"
-     , ID_LABEL = "ID"
-    
-     , SQUOTE_LABEL = "SQUOTE"
-     , DQUOTE_LABEL = "DQUOTE"
-     , COMMA_LABEL = "COMMA"
-     , SEMICOLON_LABEL = "SEMICOLON"
-     , DOT_LABEL = "DOT"
-    
-     , A_CHAR_LABEL = "A_CHAR"
-    
-     , STRING_LABEL = "STRING"
-     , FLOAT_LABEL = "FLOAT"
-     , CHARACTER_LABEL = "CHARACTER"
-     , BOOLEAN_LABEL = "BOOLEAN"
-     , INT_LABEL = "INT"
-    
-    
-     , COMMENT1_LABEL = "COMMENT1"
-     , COMMENT2_LABEL = "COMMENT2"
-     , COMMENT_L_LABEL = "unknown token"
-     , COMMENT_R_LABEL = "unknown token"
-    
-     , PLUS_LABEL = "PLUS"
-     , MINUS_LABEL = "MINUS"
-     , MULTIPLY_LABEL = "MULTIPLY"
-     , EQUAL_LABEL = "EQUAL"
-     , DIVITION_LABEL = "DIVITION"
-    
-     , LEFT_CURLY_B_LABEL = "LEFT_CURLY_B"
-     , RIGHT_CURLY_B_LABEL = "RIGHT_CURLY_B"
-     , LEFT_SQUARE_B_LABEL = "LEFT_SQUARE_B"
-     , RIGHT_SQUARE_B_LABEL = "RIGHT_SQUARE_B"
-     , LEFT_ROUND_B_LABEL = "LEFT_ROUND_B"
-     , RIGHT_ROUND_B_LABEL = "RIGHT_ROUND_B"
-     , LESSTHAN_LABEL = "LESSTHAN"
-     , GREATERTHAN_LABEL = "GREATERTHAN"
-    
-     , NOT_LABEL = "NOT"
-     , AND_LABEL = "AND"
-     , OR_LABEL = "OR";
-        /////////////////////
-        tokenLabels.add(new TokenLabel(STRING_LITERAL_LABEL , STRING_LITERAL));
-        tokenLabels.add(new TokenLabel(IF_LABEL , IF));
-        tokenLabels.add(new TokenLabel(ELSE_LABEL , ELSE));
-        tokenLabels.add(new TokenLabel(WHILE_LABEL , WHILE));
-        tokenLabels.add(new TokenLabel(MAIN_LABEL , MAIN));
-        tokenLabels.add(new TokenLabel(VOID_LABEL , VOID));
-        tokenLabels.add(new TokenLabel(THIS_LABEL , THIS));
-        tokenLabels.add(new TokenLabel(CLASS_LABEL , CLASS));
-        tokenLabels.add(new TokenLabel(PUBLIC_LABEL , PUBLIC));
-        tokenLabels.add(new TokenLabel(RETURN_LABEL , RETURN));
-        tokenLabels.add(new TokenLabel(STATIC_LABEL , STATIC));
-        tokenLabels.add(new TokenLabel(NEW_LABEL , NEW));
-        tokenLabels.add(new TokenLabel(TRUE_LABEL , TRUE));
-        tokenLabels.add(new TokenLabel(FALSE_LABEL , FALSE));
-        tokenLabels.add(new TokenLabel(LENGTH_LABEL , LENGTH));
-        tokenLabels.add(new TokenLabel(EXTENDS_LABEL , EXTENDS));
-        tokenLabels.add(new TokenLabel(SYSTEM_OUT_PRINTLN_LABEL , SYSTEM_OUT_PRINTLN));
-        tokenLabels.add(new TokenLabel(FLOAT_LITERAL_LABEL , FLOAT_LITERAL));
-        tokenLabels.add(new TokenLabel(INTEGRAL_LITERAL_LABEL , INTEGRAL_LITERAL));
-        
-        tokenLabels.add(new TokenLabel(SQUOTE_LABEL , SQUOTE));
-        tokenLabels.add(new TokenLabel(DQUOTE_LABEL , DQUOTE));
-        
-        tokenLabels.add(new TokenLabel(COMMA_LABEL , COMMA));
-        tokenLabels.add(new TokenLabel(SEMICOLON_LABEL , SEMICOLON));
-        tokenLabels.add(new TokenLabel(DOT_LABEL , DOT));
-        tokenLabels.add(new TokenLabel(A_CHAR_LABEL , A_CHAR));
-        tokenLabels.add(new TokenLabel(STRING_LABEL , STRING));
-        tokenLabels.add(new TokenLabel(FLOAT_LABEL , FLOAT));
-        tokenLabels.add(new TokenLabel(CHARACTER_LABEL , CHARACTER));
-        tokenLabels.add(new TokenLabel(BOOLEAN_LABEL , BOOLEAN));
-        tokenLabels.add(new TokenLabel(INT_LABEL , INT));
-        tokenLabels.add(new TokenLabel(COMMENT1_LABEL , COMMENT1));
-        tokenLabels.add(new TokenLabel(COMMENT2_LABEL , COMMENT2));
-        tokenLabels.add(new TokenLabel(COMMENT_L_LABEL , COMMENT_L));
-        tokenLabels.add(new TokenLabel(COMMENT_R_LABEL , COMMENT_R));
-        tokenLabels.add(new TokenLabel(PLUS_LABEL , PLUS));
-        tokenLabels.add(new TokenLabel(MINUS_LABEL , MINUS));
-        tokenLabels.add(new TokenLabel(MULTIPLY_LABEL , MULTIPLY));
-        tokenLabels.add(new TokenLabel(EQUAL_LABEL , EQUAL));
-        tokenLabels.add(new TokenLabel(DIVITION_LABEL , DIVITION));
-        tokenLabels.add(new TokenLabel(LEFT_CURLY_B_LABEL , LEFT_CURLY_B));
-        tokenLabels.add(new TokenLabel(RIGHT_CURLY_B_LABEL , RIGHT_CURLY_B));
-        tokenLabels.add(new TokenLabel(LEFT_SQUARE_B_LABEL , LEFT_SQUARE_B));
-        tokenLabels.add(new TokenLabel(RIGHT_SQUARE_B_LABEL , RIGHT_SQUARE_B));
-        tokenLabels.add(new TokenLabel(LEFT_ROUND_B_LABEL , LEFT_ROUND_B));
-        tokenLabels.add(new TokenLabel(RIGHT_ROUND_B_LABEL , RIGHT_ROUND_B));
-        tokenLabels.add(new TokenLabel(LESSTHAN_LABEL , LESSTHAN));
-        tokenLabels.add(new TokenLabel(GREATERTHAN_LABEL , GREATERTHAN));
-        tokenLabels.add(new TokenLabel(NOT_LABEL , NOT));
-        tokenLabels.add(new TokenLabel(AND_LABEL , AND));
-        tokenLabels.add(new TokenLabel(OR_LABEL , OR));
-        tokenLabels.add(new TokenLabel(EOF_LABEL , EOF));
-        tokenLabels.add(new TokenLabel(ID_LABEL , ID));
+            /** AZZAM STARTED FROM HERE  **/ 
+       
+	   , STATIC_LABEL = "STATIC"
+           , NEW_LABEL = "NEW" 
+	   , STRING_LABEL = "STRING"
+           , FLOAT_LABEL = "FLOAT"
+           , CHARACTER_LABEL = "CHARACTER"
+           , BOOLEAN_LABEL = "BOOLEAN"
+	   , EXTENDS_LABEL = "EXTENDS"
+           , NULL_LABEL = "NULL"
+           , ABSTRACT_LABEL = "ABSTRACT" 
+           , ASSERT_LABEL = "ASSERT"
+           , BREAK_LABEL = "BREAK"
+           , CONTINUE_LABEL = "CONTINUE"
+           , BYTE_LABEL = "BYTE"    
+           , CASE_LABEL = "CASE" 
+           , CATCH_LABEL = "CATCH"
+           , CONST_LABEL = "CONST"
+           , DEFAULT_LABEL = "DEFAULT"
+           , DO_LABEL = "DO"
+           , DOUBLE_LABEL = "DOUBLE"
+           , ENUM_LABEL = "ENUM"
+           , FINAL_LABEL = "FINAL"  
+             
+            /// AZZAM END HERE            
+             
+             ;
+        ////////////////////////////////////////////////////////////////////////
+		
+		
+        /**  MAZEN START HERE **/
+
+          tokenLabels.add(new TokenLabel( MOD_LABEL , MOD ) ) ;
+          tokenLabels.add(new TokenLabel( LESSTHAN_LABEL , LESSTHAN ) ) ;
+          tokenLabels.add(new TokenLabel( GREATERTHAN_LABEL , GREATERTHAN ) ) ;
+          tokenLabels.add(new TokenLabel( LESS_EQ_LABEL , LESS_EQ ) ) ;
+          tokenLabels.add(new TokenLabel( GREATER_EQ_LABEL , GREATER_EQ ) ) ;
+          tokenLabels.add(new TokenLabel( IF_LABEL , IF ) ) ;
+          tokenLabels.add(new TokenLabel( INT_LABEL , INT ) ) ;
+          tokenLabels.add(new TokenLabel( ELSE_LABEL , ELSE ) ) ;
+          tokenLabels.add(new TokenLabel( MAIN_LABEL , MAIN ) ) ;
+          tokenLabels.add(new TokenLabel( THIS_LABEL , THIS ) ) ;
+          tokenLabels.add(new TokenLabel( TRUE_LABEL , TRUE ) ) ;
+          tokenLabels.add(new TokenLabel( VOID_LABEL , VOID ) ) ;
+          tokenLabels.add(new TokenLabel( CLASS_LABEL , CLASS ) ) ;
+          tokenLabels.add(new TokenLabel( FALSE_LABEL , FALSE ) ) ;
+          tokenLabels.add(new TokenLabel( WHILE_LABEL , WHILE ) ) ;
+          tokenLabels.add(new TokenLabel( LENGTH_LABEL , LENGTH ) ) ;
+          tokenLabels.add(new TokenLabel( PUBLIC_LABEL , PUBLIC ) ) ;
+          tokenLabels.add(new TokenLabel( PRIVATE_LABEL , PRIVATE ) ) ;
+          tokenLabels.add(new TokenLabel( PROTECTED_LABEL , PROTECTED ) ) ;
+          tokenLabels.add(new TokenLabel( RETURN_LABEL , RETURN ) ) ;
+
+          /** MAZEN END HERE **/
+
+          /**  AZZAM START HERE **/
+
+          tokenLabels.add(new TokenLabel(STATIC_LABEL , STATIC));
+          tokenLabels.add(new TokenLabel(NEW_LABEL , NEW));
+          tokenLabels.add(new TokenLabel(STRING_LABEL , STRING));
+          tokenLabels.add(new TokenLabel(FLOAT_LABEL , FLOAT));
+          tokenLabels.add(new TokenLabel(CHARACTER_LABEL , CHARACTER));
+          tokenLabels.add(new TokenLabel(BOOLEAN_LABEL , BOOLEAN));
+          tokenLabels.add(new TokenLabel(EXTENDS_LABEL , EXTENDS));
+          tokenLabels.add(new TokenLabel( NULL_LABEL , NULL ) ) ;
+          tokenLabels.add(new TokenLabel( ABSTRACT_LABEL , ABSTRACT ) ) ;
+          tokenLabels.add(new TokenLabel( ASSERT_LABEL , ASSERT ) ) ;
+          tokenLabels.add(new TokenLabel( BREAK_LABEL , BREAK ) ) ;
+          tokenLabels.add(new TokenLabel( CONTINUE_LABEL , CONTINUE ) ) ;
+          tokenLabels.add(new TokenLabel( BYTE_LABEL , BYTE ) ) ;
+          tokenLabels.add(new TokenLabel( CASE_LABEL , CASE ) ) ;
+          tokenLabels.add(new TokenLabel( CATCH_LABEL , CATCH ) ) ;
+          tokenLabels.add(new TokenLabel( CONST_LABEL ,CONST ) ) ;
+          tokenLabels.add(new TokenLabel( DEFAULT_LABEL , DEFAULT ) ) ;
+          tokenLabels.add(new TokenLabel( DO_LABEL, DO ) ) ;
+          tokenLabels.add(new TokenLabel( DOUBLE_LABEL , DOUBLE ) ) ;
+          tokenLabels.add(new TokenLabel( ENUM_LABEL , ENUM) ) ;
+          tokenLabels.add(new TokenLabel( FINAL_LABEL , FINAL ) ) ;
+
+          /** AZZAM END HERE **/
+
         return tokenLabels;
         
     }
